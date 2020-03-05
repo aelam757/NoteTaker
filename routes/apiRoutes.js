74b09c2e3ff7b8777d55027e4be5ee86083db717
Alan Elam <aelam757@gmail.com>
2020-03-04 21:02:11 -0500

    adding route structure

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,13 @@
-const notes = require("../data/notes")
+const notes = require("../data/notes");
+
+function setAPIRoutes (app) {
+    app.get("/api/notes", (req,res)=>{
+        res.json(notes);
+    });
+
+    app.post("/api/notes", (req,res) =>{
+        res.json(notes);
+    });
+}
+
+module.exports = setAPIRoutes;
