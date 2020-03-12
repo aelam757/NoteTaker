34c142b38f6436305ac170c52c90a04776cc56bd
Alan Elam <aelam757@gmail.com>
2020-03-11 22:45:45 -0400

    updating file

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
 
 const app = express();
 
@@ -8,9 +7,9 @@ const PORT = process.env.PORT || 3080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get
 
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`);
-})
+});
