8e03f10b2274667ddb3cdb977724f578c5c5feb7
Alan Elam <aelam757@gmail.com>
2020-03-15 23:29:46 -0400

    deploying app to heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,5 @@
-const notes = require("../data/notes");
 
-function setAPIRoutes (app) {
+
     app.get("/api/notes", (req,res)=>{
         res.json(notes);
     });
@@ -8,6 +7,3 @@ function setAPIRoutes (app) {
     app.post("/api/notes", (req,res) =>{
         res.json(notes);
     });
-}
-
-module.exports = setAPIRoutes;
