fba7fe24afe8d065eea166dfc816f546d39ce5d4
Alan Elam <aelam757@gmail.com>
2020-05-21 20:16:46 -0400

    adding js and css to public

--- data/notes.js
+++ data/notes.js
@@ -1,3 +1,41 @@
-const notes = [];
+const util = require("util");
+const fs = require("fs");
+const uuidv1 = require("uuid/v1");
 
-export default notes;
+const readFileAsync = util.promisify(fs.readFile)
+const writeFileAsync = util.promisify(fs.writeFile)
+
+class Note {
+    read(){
+        return readFileAsync("data/db.json", "utf8")
+    }
+    write(note){
+        return writeFileAsync("data/db.json", JSON.stringify(note))
+    }
+    getNotes(){
+        return this.read().then(notes =>{
+            let parseNotes; 
+            try{
+                parseNotes = [].concat(JSON.parse(notes))
+            }
+            catch(error){parseNotes=[]
+            }
+            return parseNotes
+        }
+    )}
+    addNote(note){
+        const {title,text} = note 
+        if (!title || !text){
+            throw new Error("Can't be blank")
+        }
+        const newNote = {title,text,id:uuidv1()}
+        return this.getNotes().then(notes =>
+            [...notes,newNote]).then(uNotes => this.write(uNotes)).then(()=> newNote)
+    }
+    removeNote(id){
+        return this.getNotes().then(notes=>
+            notes.filter(note => note.id!==id)).then(fNotes => this.write(fNotes))
+    }
+};
+
+module.exports = new Note;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,22 @@
-    app.get("/api/notes", (req,res)=>{
-        res.json(notes);
-    });
+const router = require("express").Router();
+const dataN = require("../data/notes")
 
-    app.post("/api/notes", (req,res) =>{
-        res.json(notes);
-    });
+router.get("/notes",function(req,res){
+    notes.getNotes().then((note)=>
+    res.json(note))
+    .catch(error => res.status(500).json(error))
+});
+
+router.post("/notes",function(req,res){
+    notes.addNote(req.body).then((note)=>
+    res.json(note))
+    .catch(error => res.status(500).json(error))
+});
+
+router.delete("/notes",function(req,res){
+    notes.removeNote(req.params.id).then(()=>
+    res.json({ok:true}))
+    .catch(error => res.status(500).json(error))
+});
+
+module.exports = router;
